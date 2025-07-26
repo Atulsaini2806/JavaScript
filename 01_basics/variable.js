38a5b97e65282798ca52ae8aadf3571affacc2e6
Atulsaini2806 <atulsaini10123@gmail.com>
2025-07-26 11:25:23 +0530

    variable

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -1,1 +1,8 @@
-console.log("engineering")
+const accountId = 11432536
+let accountEmail = "atul@miet.com"
+var accountPassward = "2132"
+accountCity = "Delhi"
+
+accountId = 414325
+
+console.log("accountId");
