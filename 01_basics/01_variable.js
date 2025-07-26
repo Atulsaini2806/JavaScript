b4f14cb80dcc9a19e829e6464dba5eea6edc06a4
Atulsaini2806 <atulsaini10123@gmail.com>
2025-07-26 23:17:13 +0530

    variable

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,8 +1,17 @@
 const accountId = 11432536
 let accountEmail = "atul@miet.com"
 var accountPassward = "2132"
-accountCity = "Delhi"
+accountCity = "Delhi" /*not a good choice*/
 
-accountId = 12345
+// accountId = 12345// Not Allow
 
-console.log("accountId");
+console.log(accountId);
+accountEmail = "atul@gmail.com"
+varaccountPssward = "6789"
+accountCity = "Noida"
+
+/*Prefer not yo use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId , accountEmail, accountPassward ,accountCity]);
+
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,38 @@
+"use strict" //treat all js code as newer version
+
+//alert(3+3)// we are using node js Not browser 
+console.log(3 + 
+    3);
+  // code readability should be high//
+console.log("atul Saini");
+
+
+let name = "Atul Saini"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+
+// There are two types of datatypes that is primitive and non-primitive(refrence type)
+
+//Primitive datatypes:- they are call by value and have 7 types
+//number => 2 to power 53
+//bigint => specially used for stock market and trading(big number)
+//stri ng => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol =>uniqueness 
+
+
+//object
+console.log(typeof "Atul Saini");
+console.log(typeof 18);
+console.log(typeof false);
+console.log(typeof null);   //output = object
+console.log(typeof undefined);  //output = undefined
+
+
+
+
+
