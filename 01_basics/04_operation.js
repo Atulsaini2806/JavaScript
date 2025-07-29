8de3915975ede0796e5f4d34b6b21c43a9522d7f
Atulsaini2806 <atulsaini10123@gmail.com>
2025-07-29 11:47:40 +0530

    Operations

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,29 @@
+let value = 9
+let negvalue = -value
+console.log(negvalue);
+// There are multiple opertions in js some of them here......
+   console.log(4+4);
+   console.log(4*4);
+   console.log(4-4);
+   console.log(7%4);
+   console.log(4/4);
+   console.log(4**3);
+
+
+   let str1 = "Hello"
+   let str2 = "Everyone"
+      console.log(str1+str2);//HellEveryone
+
+
+     console.log("1" + 4);
+     console.log(1 + "3");
+     console.log("1" + 2 + 3);//if first element is string then entire element treat as string.
+     console.log(5 - 3 +"3");  //if last element is string thent first apply the given opertion
+                          //  on that element that comes before the last element(string) 
+                          // then add the the last element as a string.
+
+
+        let ameCounter =  200   
+        ameCounter++;
+        console.log(ameCounter);
+            
