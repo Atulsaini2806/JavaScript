3d30f63d6b930cfd67ee82d8ae8e62d4c7cf54d9
Atulsaini2806 <atulsaini10123@gmail.com>
2025-07-31 01:44:43 +0530

    operation

--- 01_basics/04_operation.js
+++ 01_basics/04_operation.js
@@ -2,12 +2,12 @@ let value = 9
 let negvalue = -value
 console.log(negvalue);
 // There are multiple opertions in js some of them here......
-   console.log(4+4);
-   console.log(4*4);
-   console.log(4-4);
-   console.log(7%4);
-   console.log(4/4);
-   console.log(4**3);
+   console.log(4+4);//8
+   console.log(4*4);//16
+   console.log(4-4);//0
+   console.log(7%4);//3(remender)
+   console.log(11/4);//2
+   console.log(4**3);//4*4*4=64
 
 
    let str1 = "Hello"
