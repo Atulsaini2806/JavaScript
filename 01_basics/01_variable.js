0204bab6246c4ecb3f36ae24446fabf507fe6307
Atulsaini2806 <atulsaini10123@gamil.com>
2025-09-27 22:58:14 +0530

    variable

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,3 +1,4 @@
+//In js const, let , var are used to declare a variable but they have different scopes rules and behavooyr.
 const accountId = 11432536
 let accountEmail = "atul@miet.com"
 var accountPassward = "2132"
@@ -6,6 +7,7 @@ accountCity = "Delhi" /*not a good choice*/
 // accountId = 12345// Not Allow
 
 console.log(accountId);
+console.log(accountEmail);
 accountEmail = "atul@gmail.com"
 accountPssward = "6789"
 accountCity = "Noida"
@@ -15,3 +17,7 @@ because of issue in block scope and functional scope
 */
 console.table([accountId , accountEmail, accountPassward ,accountCity]);
 
+const name = "aarjun"
+console.log(name); 
+
+//name = "Atul" // not allow
