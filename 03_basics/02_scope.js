cadd5120b365dde69ef38ec8eb3695df29ac413a
Atulsaini2806 <atulsaini10123@gamil.com>
2025-09-29 01:57:58 +0530

    scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,19 +1,20 @@
 // Scope within {} is called block scope and outside scope is called global scope like below
 
-       let a = 300;
-       const b =90;
-       var c = 40;
+      let a = 300;
+    const b =90;
+     var c = 40;
 // these are global scope
 
     if(true){
        let a=20;
        const b=30;
-       var c =22;
+        var c =22;
 // this is called block scope
      console.log("inner:", a);//20
      console.log(b);//30
 }
      console.log(a);//300
+    console.log(b);//90
      console.log(c);//22 b'coz var is changeble within scope
 // block scope take value from global scope but it does not give it's value or leak value of it's own.
 
@@ -34,6 +35,7 @@
         //or
      if(true){
          const username = "Atul";
+          console.log(username);
          if(username==="Atul"){
              const website = "youtube";
              console.log(username + website);
@@ -41,19 +43,21 @@
          //console.log(website);
          
      }
-      // console.log(username);
+       //console.log(username);
 
 
       
-//+++++++++++++++interesting concept +++++++++++++
+// //+++++++++++++++interesting concept +++++++++++++
 
-console.log(addOne(5));//Here it is executed.
-function addOne(num){
-    return num+1;
-}
+// console.log(addOne(5));//Here it is executed.
+// function addOne(num){
+//     return num+1;
+// }
 
-// or
-addTwo(7)//Here it does not execute b'coz of declaration method which is called hosting(discus it into later part)
-const addTwo=function(num){
-    return num+2;
-}
+// // or
+// addTwo(7)//Here it does not execute b'coz of declaration method which is called hosting(discus it into later part)
+// const addTwo=function(num){
+//     return num+2;
+// }
+
+// //note : {} is called scope with function and control flow statement but with object.
